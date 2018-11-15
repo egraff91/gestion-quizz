51b445169076e81b9e6ed8467a881d551062a088
Etienne <etienne.graff@hotmail.fr>
2018-11-15 10:54:53 +0100

    TP3-POE

--- src/fr/diginamic/console/QuizzAdminConsoleApp.java
+++ src/fr/diginamic/console/QuizzAdminConsoleApp.java
@@ -10,7 +10,7 @@ public class QuizzAdminConsoleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Question> questions = new ArrayList();
+		ArrayList<Question> questions = new ArrayList<Question>();
 		
 		
 		Question question1 = new Question("Quelle est la capitale de la France ?", "Paris");
@@ -29,10 +29,11 @@ public class QuizzAdminConsoleApp {
 		questions.add(question2);
 		
 		Scanner questionUser = new Scanner(System.in);
+		
 		boolean sortir = false;
 		while (!sortir) {
 			afficheMenu();
-			int a = questionUser.nextInt();
+			int a = Integer.parseInt(questionUser.nextLine());
 		
 			switch(a) {
 				case 1: System.out.println("Liste des questions");
@@ -40,12 +41,15 @@ public class QuizzAdminConsoleApp {
 					
 					break;
 					case 2: System.out.println("Ajout d'une nouvelle question");
+					questions.add(nouvelleQuestion(questionUser));
 					
 					break;
 					case 3: System.out.println("Suppression d'une question");
+					supprimeQuestion(questions, questionUser);
 					
 					break;
 					case 4: System.out.println("Exécution du quizz");
+					startQuizz(questions, questionUser);
 					
 					break;
 					case 99: System.out.println("Au revoir");
@@ -66,13 +70,61 @@ public class QuizzAdminConsoleApp {
 	
 	public static void afficheQuestions(ArrayList<Question> questions) {
 		for(int i=0; i<questions.size();i++) {
-			System.out.println(i+1+") "+questions.get(i).getIntitule());
-			for(int j=0; j<questions.get(i).getPropositions().size();j++) {
-				System.out.println("     - "+questions.get(i).getPropositions().get(j));
-			}
-			
+			afficheQuestion(i+1,questions.get(i));
 		}
 		
+	}
+	
+	public static void afficheQuestion(int numero, Question question) {
+		System.out.println(numero+") "+question.getIntitule());
+		for(int j=0; j<question.getPropositions().size();j++) {
+			System.out.println("     - "+question.getPropositions().get(j));
+		}
+	}
+	
+	public static Question nouvelleQuestion(Scanner questionUser) {
+		ArrayList<String> propositions = new ArrayList<String>();
+		System.out.println("Veuillez saisir l'intitulé de la question: ");
+		String intitule = questionUser.nextLine();
+		System.out.println("Veuillez saisir le nombre de réponses: ");
+		int nombreReponses = Integer.parseInt(questionUser.nextLine());
+		for (int i= 0; i<nombreReponses; i++) {
+			System.out.println("Veuillez saisir la proposition de réponse "+(i+1)+":");
+			propositions.add(questionUser.nextLine());
+		}
+		System.out.println("Veuillez saisir le numéro de la bonne réponse (entre 1 et "+nombreReponses+"):");
+		int indexReponse = Integer.parseInt(questionUser.nextLine());
+		//System.out.println(indexReponse+" "+propositions);
+		String bonneReponse = propositions.get(indexReponse-1);
+		
+		Question nouvelleQuestion = new Question(intitule,bonneReponse);
+		//System.out.println("test");
+		nouvelleQuestion.setPropositions(propositions);
+		//demandeQuestion.close();
+		return nouvelleQuestion;
+	}
+	
+	public static void supprimeQuestion(ArrayList<Question> questions, Scanner questionUser) {
+		System.out.println("Veuillez indiquer le numéro de la question à supprimer (entre 1 et "+questions.size()+")");
+		int indexSuppr = Integer.parseInt(questionUser.nextLine());
+		questions.remove(indexSuppr-1);
+		
+	}
+	
+	public static void startQuizz(ArrayList<Question> questions, Scanner questionUser) {
+		int score = 0;
+		for(int i= 0; i<questions.size();i++) {
+			afficheQuestion(i+1, questions.get(i));
+			System.out.println("Veuillez choisir une réponse");
+			String reponse = questionUser.nextLine();
+			if(questions.get(i).verifierReponse(reponse)) {
+				System.out.println("Bonne réponse");
+				score++;
+			}else {
+				System.out.println("Mauvaise réponse");
+			}
+		}
+		System.out.println("Votre score: "+score);
 	}
 
 }
